aae24c17101973934f2c24c2a20af1678732123c
David <david_buddy@hotmail.de>
2023-04-20 18:07:30 +0200

    Final commit, finished loading snail data

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dataset import BirdDataset
 def main():
     bird_dataset = BirdDataset(root_dir='data',csv_file='data/birds.csv',transform=None)
     #Load snail data
+    #Finally done with programming
    
     for i in range(len(bird_dataset)):
         sample = bird_dataset[i]
